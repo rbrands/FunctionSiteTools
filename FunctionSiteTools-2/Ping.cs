c6aebc7198e3d2664557102eabfb67129739554f
Robert Brands (RiwaAdmin) <rbadmin@riwa4.de>
2018-12-12 11:21:27 +0100

    Date added

--- FunctionSiteTools-2/Ping.cs
+++ FunctionSiteTools-2/Ping.cs
@@ -34,7 +34,7 @@ namespace FunctionSiteTools
             name = name ?? data?.name;
 
             return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
+                ? (ActionResult)new OkObjectResult($"Hello, {name} 2018-12-12")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
     }
